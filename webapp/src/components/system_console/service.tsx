ab09cd2df73f1d9a3a79152c3e93bb9b467d6fbe
Vlastimil Pečínka <vlastimil.pecinka@firma.seznam.cz>
2026-01-17 01:51:27 +0100

    move custom headers from bot to service

--- webapp/src/components/system_console/service.tsx
+++ webapp/src/components/system_console/service.tsx
@@ -14,6 +14,7 @@ import {ButtonIcon} from '../assets/buttons';
 import {fetchModels} from '../../client';
 
 import {BooleanItem, ItemList, SelectionItem, SelectionItemOption, TextItem, ComboboxItem} from './item';
+import {CustomHeadersItem} from './custom_headers';
 
 export type LLMService = {
     id: string
